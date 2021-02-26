013cc99fc96138d894fffc31762699265750c5eb
Anindya-2000 <sikdarani01@gmail.com>
2021-02-26 19:58:09 +0530

    added router in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import Questions from './components/QuestionsPage/Questions';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <Questions/>
+    <Router>
+      <Switch>
+        {/* <Route exact path="/">
+            <Home />
+        </Route> */}
+        <Route path="/play">
+            <Questions/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
